be3123d02556acb2b567bde3fe0f951742572dea
unknown <bassem.labbassi@gmail.com>
2020-04-16 22:36:08 +0100

    adding the userSchema

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require('express'); 
 const mongoose = require("mongoose");
 const { MONGOURI } = require('./config/keys');
+const User = require('./Models/User')
 
 
-mongoose.connect(MONGOURI,{ useNewUrlParser: true, useUnifiedTopology: true},(err)=>{
+mongoose.connect( MONGOURI,{ useNewUrlParser: true, useUnifiedTopology: true},(err)=>{
     if(err) throw err; 
-    console.log('connected to tha data base')
-})
+    console.log('successfully connected to the data base')
+});
 
 
 
@@ -17,8 +18,7 @@ const app = express();
 
 app.get('/', (req,res)=>{
 res.send("hello to my channel")
-
-})
+        })
 
 
 
