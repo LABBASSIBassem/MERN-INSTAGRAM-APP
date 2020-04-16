db4dae6ecce75f0167fbbb6bfcc77ac1520a5d2b
unknown <bassem.labbassi@gmail.com>
2020-04-16 21:47:00 +0100

    connect the project to mongoDb

--- server/app.js
+++ server/app.js
@@ -1,6 +1,18 @@
 const express = require('express'); 
+const mongoose = require("mongoose");
+const { MONGOURI } = require('./config/keys');
+
+
+mongoose.connect(MONGOURI,{ useNewUrlParser: true, useUnifiedTopology: true},(err)=>{
+    if(err) throw err; 
+    console.log('connected to tha data base')
+})
+
+
 
 const app = express(); 
+
+
 
 
 app.get('/', (req,res)=>{
